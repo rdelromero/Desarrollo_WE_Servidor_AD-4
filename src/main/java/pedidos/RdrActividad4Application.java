ed84f826fdba200f411f3b1af6d2fb7cf0b809a4
Ricardo <124194247+rdelromero@users.noreply.github.com>
2024-03-01 23:58:41 +0100

    Add files via upload

---
+++ src/main/java/pedidos/RdrActividad4Application.java
@@ -0,0 +1,12 @@
+package pedidos;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdrActividad4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdrActividad4Application.class, args);
+	}
+
+}
---
+++ src/test/java/pedidos/RdrActividad4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdrActividad4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
